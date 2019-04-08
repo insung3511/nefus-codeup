cde25d05d95e316525c484ade6c43b59a1b92756
박인성 (Bahk InSung) <insung.park123@gmail.com>
2019-04-08 16:49:10 +0900

    Create 1471.c

---
+++ 1471.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main() {
+    int count = 0;
+    int arr[100][100] = { '\n' };
+    int num = 0;
+    
+    //printf("> ");
+    scanf("%d", &count);
+    
+    for (int j = 0; j < count; j++) {
+        if (j % 2 == 0) {
+            
+            for (int i = count - 1; i >= 0; i--) {
+                num += 1;
+                arr[i][j] = num;
+            }
+        }
+        else {
+            for (int i = 0; i < count; i++) {
+                num += 1;
+                arr[i][j] = num;
+            }
+        }
+        
+    }
+    for (int i = 0; i < count; i++) {
+        for (int j = 0; j < count; j++) {
+            printf("%d ", arr[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
+
